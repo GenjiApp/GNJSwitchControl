e8818c20d8adaa556ac7b7ca0403152296c764c5
Genji <chaos.nonlinear@gmail.com>
2015-10-14 22:43:24 +0900

    Initial Commit

---
+++ GNJSwitchControl/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GNJSwitchControl
+//
+//  Created by Genji on 2015/10/14.
+//  Copyright © 2015 Genji App. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+    return true
+  }
+
+}
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# GNJSwitchControl
+
+`GNJSwitchControl` is a subclass of `NSControl` which has on/off state. By clicking a switch control or dragging a knob to change state.
+
+![](demo.gif)
+
+## Usage
+
+1. Add a Custom View (`NSView`) to other view in Interface Builder/Storyboard.
+2. Set the Custom Class to `GNJSwitchControl`.
